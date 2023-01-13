06308f8d23734a6480609bf71fd6607d31e0599e
hankliao87 <hankliao87@gmail.com>
2023-01-13 22:11:30 +0800

    Update python api to version 0.5

--- __init__.py
+++ __init__.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
-"""Web search in brave incognito mode
-Synopsis: <trigger> <expression>"""
 
 import time
 import urllib
+import urllib.parse
 
 from albert import *
 
-__title__ = "Web Search"
-__version__ = "0.4.0"
-__triggers__ = "!"
-__authors__ = ["hankliao87"]
+__doc__ = """
+Web search in brave incognito mode
+Synopsis: `<trigger> <expression>`
+"""
+
+md_iid = "0.5"
+md_version = "0.1"
+md_name = "Web Search"
+md_description = "Web Search"
+md_license = "MIT"
+md_url = "https://github.com/hankliao87/albert-web-search"
+md_maintainers = "@hankliao87"
+
+trigger = "!"
 
 EXEC = "brave"
 
@@ -22,50 +31,122 @@ BANG_DICT = {
     "dd": {"name": "DuckDuckGo", "url": "https://duckduckgo.com/?q={}"},
 }
 
-iconPath = iconLookup(EXEC)
 
-def handleQuery(query):
-    if query.isTriggered:
+class Plugin(QueryHandler):
+    iconPath = [f"xdg:{EXEC}"]
+
+    def id(self):
+        return __name__
+
+    def name(self):
+        return md_name
+
+    def description(self):
+        return md_description
+
+    def synopsis(self) -> str:
+        return "<name>"
+
+    def defaultTrigger(self) -> str:
+        return trigger
+
+    def handleQuery(self, query):
         query_string = query.string.strip()
 
         if query_string == "":
-            item = Item(id=__title__, icon=iconPath)
+            item = Item(id=__name__, icon=self.iconPath)
             item.text = "Search"
-            return item
+            query.add(item)
         else:
             try:
                 query_list = query_string.split(" ", 1)
 
                 if len(query_list) == 1:
                     bang = query_string
-                    keyword = None
+
+                    if bang == "?":
+                        for key in sorted(BANG_DICT.keys()):
+                            name = BANG_DICT[key]["name"]
+                            item = Item(
+                                id=__name__,
+                                icon=self.iconPath,
+                                text=f"{trigger}{key}",
+                                subtext=f"Search in {name}",
+                            )
+                            query.add(item)
+
+                    elif not query.string.endswith(" "):
+                        filter_BANG = list(filter(lambda item: item.startswith(bang), BANG_DICT.keys()))
+                        for key in sorted(filter_BANG):
+                            name = BANG_DICT[key]["name"]
+                            item = Item(
+                                id=__name__,
+                                icon=self.iconPath,
+                                text=f"{trigger}{key}",
+                                subtext=f"Search in {name}",
+                            )
+                            query.add(item)
+
+                    else:
+                        if BANG_DICT.get(bang, {}).get("name", None):
+                            name = BANG_DICT[bang]["name"]
+                            url = BANG_DICT[bang]["url"]
+                        else:
+                            raise Exception("Service not found")
+
+                        item = Item(id=__name__, icon=self.iconPath)
+                        item.text = f"Search in {name}"
+                        query.add(item)
+
                 else:
                     bang, keyword = query_list
 
-                if BANG_DICT.get(bang, {}).get("name", None):
-                    name = BANG_DICT[bang]["name"]
-                    url = BANG_DICT[bang]["url"]
-                else:
-                    raise Exception("Service not found")
+                    if BANG_DICT.get(bang, {}).get("name", None):
+                        name = BANG_DICT[bang]["name"]
+                        url = BANG_DICT[bang]["url"]
+                    else:
+                        raise Exception("Service not found")
 
-                if keyword:
                     # time.sleep(0.3) # avoid rate limiting
                     api_url = url.format(urllib.parse.quote_plus(keyword))
+                    item = Item(
+                        id=__name__,
+                        icon=self.iconPath,
+                        text=str(keyword),
+                        subtext=f"Search in {name}",
+                        actions=[
+                            Action(
+                                id="open-incognito",
+                                text="Open in incognito mode",
+                                callable=lambda u=[EXEC, "-incognito", str(api_url)]: runDetachedProcess(u)),
+                            Action(
+                                id="open-normal",
+                                text="Open in normal mode",
+                                callable=lambda u=[EXEC, str(api_url)]: runDetachedProcess(u)),
+                            Action(
+                                id="copy-repo-url",
+                                text="Copy repo url to clipboard",
+                                callable=lambda u=api_url: setClipboardText(u)),
+                        ]
+                    )
+                    query.add(item)
 
             except Exception as ex:
-                item = Item(id=__title__, icon=iconPath)
-                item.text = "Error: " + str(ex)
-                item.addAction(ClipAction(text="Copy error message to clipboard", clipboardText=str(ex)))
-                return item
-
-            item = Item(id=__title__, icon=iconPath)
-            if keyword:
-                item.text = str(keyword)
-                item.subtext = f"Search in {name}"
-                item.addAction(ProcAction(text="Open in brave incognito mode", commandline=[EXEC, "-incognito", f"{api_url}"]))
-                item.addAction(ProcAction(text="Open in brave normal mode", commandline=[EXEC, f"{api_url}"]))
-                item.addAction(ClipAction(text="Copy url to clipboard", clipboardText=str(api_url)))
-            else:
-                item.text = f"Search in {name}"
-            return item
+                item = Item(
+                    id=__name__,
+                    icon=self.iconPath,
+                    actions=[
+                        Action(
+                            id="copy-clipboard",
+                            text="Copy error message to clipboard",
+                            callable=lambda u=str(ex): setClipboardText(u)),
+                        Action(
+                            id="copy-repo-url",
+                            text="Copy repo url to clipboard",
+                            callable=lambda: setClipboardText(md_url)),
+                    ]
+                )
+                item.text = f"Error: {str(ex)}"
+                item.subtext = f"Please create an issue in {md_url}"
+                query.add(item)
 
